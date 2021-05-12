3f6e360edddf11a2c6dd3b655a9ab2e06b3dd216
dobkir <dobkir@gmail.com>
2021-05-12 21:16:10 +0400

    Express.js. Basic application setup

--- index.js
+++ index.js
@@ -1,65 +1,8 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs')
-// Pure web-server
-const server = http.createServer((req, res) => {
+const express = require('express');
+const app = express();
 
-  if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
+const PORT = process.env.PORT || 3000;
 
-    if (req.url === '/') {
-      fs.readFile(
-        path.join(__dirname, 'views', 'index.html'),
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            throw err
-          }
-          res.end(content);
-        }
-      );
-    } else if (req.url === '/about') {
-      fs.readFile(
-        path.join(__dirname, 'views', 'about.html'),
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            throw err
-          }
-          res.end(content);
-        }
-      );
-    } else if (req.url === '/api/users') {
-      res.writeHead(200, {
-        'Content-Type': 'text/json'
-      });
-
-      const users = [
-        { name: 'George', surname: 'Washington' },
-        { name: 'Theodore', surname: 'Roosevelt' }
-      ];
-
-      res.end(JSON.stringify(users));
-    }
-  } else if (req.method === 'POST') {
-    const body = [];
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-    req.on('data', data => {
-      body.push(Buffer.from(data))
-    });
-    req.on('end', () => {
-      const message = body.toString().split('=')[1];
-      res.end(`<h1>Your message: ${message}</h1>`);
-    });
-  }
-});
-
-
-server.listen(3000, () => {
-  console.log('Server is running...')
-});
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
