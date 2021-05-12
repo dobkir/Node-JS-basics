1816e1860292101f964ecc3446633a2d897c9ef9
dobkir <dobkir@gmail.com>
2021-05-12 16:20:47 +0400

    Creating a simple server

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const http = require('http');
 
 // Pure web-server
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.write('<h1>Hello from NodeJS</h1>');
-  res.write('<h2>Hello from NodeJS</h2>');
-  res.write('<h3>Hello from NodeJS</h3>');
-  res.end(`
-  <div style="background: red; width: 200px; height: 200px;">
-    <h1>Test 2</h1>
-  </div>
-  `);
+
+  if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    res.end(`
+      <h1>Form</h1>
+      <form method="post" action="/">
+        <input name="title" type="text" />
+        <button type="submit">Send</button>
+      </form>
+      `)
+  } else if (req.method === 'POST') {
+    const body = [];
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    req.on('data', data => {
+      body.push(Buffer.from(data))
+    });
+    req.on('end', () => {
+      const message = body.toString().split('=')[1];
+      res.end(`<h1>Your message: ${message}</h1>`);
+    });
+  }
 });
+
 
 server.listen(3000, () => {
   console.log('Server is running...')
