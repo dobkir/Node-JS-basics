bd1d5e0cf457eb578ee7e1bb537eca3cc94be9f4
dobkir <dobkir@gmail.com>
2021-05-13 22:19:11 +0400

    Connection of express-handlevars library

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const path = require('path');
+const expressHandlebars = require('express-handlebars');
+
 const app = express();
 
+const handlebars = expressHandlebars.create({
+  defaultLayout: 'main',
+  extname: 'hbs'  // extension name
+});
+
+// register in express such engine 'handlebars'
+app.engine('hbs', handlebars.engine);
+// started to use engine 'handlebars'
+app.set('view engine', 'hbs');
+// path to directory with templates
+app.set('views', 'views');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+  res.render('index');
+});
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
-})
+  res.render('about');
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
-})
+});
