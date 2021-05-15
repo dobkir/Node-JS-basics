f83270a0a8236beb3666ff9bb863405caae61acb
dobkir <dobkir@gmail.com>
2021-05-15 23:25:03 +0400

    Data rendering: titles, routers, active link

--- index.js
+++ index.js
@@ -18,11 +18,25 @@ app.set('views', 'views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Home page',
+    isHome: true,
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Courses',
+    isCourses: true,
+  });
+});
+
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Add course',
+    isAdd: true,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
